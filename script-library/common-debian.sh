e90e9724339fee3d5456638d641c0bd7877061e2
Chuck Lantz <clantz@microsoft.com>
2019-11-16 20:52:02 -0800

    Script tweaks

--- script-library/common-debian.sh
+++ script-library/common-debian.sh
@@ -4,12 +4,12 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 
-# Syntax: ./common-debian.sh <username> <user UID> <user GID> <install zsh flag>
+# Syntax: ./common-debian.sh <install zsh flag> <username> <user UID> <user GID>
 
-USERNAME=$1
-USER_UID=$2
-USER_GID=$3
-INSTALL_ZSH=$4
+INSTALL_ZSH=$1
+USERNAME=$2
+USER_UID=$3
+USER_GID=$4
 
 set -e
 
@@ -40,7 +40,6 @@ apt-get -y install --no-install-recommends \
 # Install libssl1.0.x depending on version if it is available
 LIBSSL=$(dpkg-query -f '${db:Status-Abbrev}\t${binary:Package}\n' -W 'libssl1\.0\.?' 2>&1)
 if [ "$(echo "$LIBSSL" | grep -o 'libssl1\.0\.[0-9]:' | uniq | sort | wc -l)" -eq 0 ]; then
-    # No libssl install 1.0.2 for Debian, 1.0.0 for Ubuntu
     if [[ ! -z $(apt-cache --names-only search ^libssl1.0.2$) ]]; then
         apt-get -y install  --no-install-recommends libssl1.0.2
     else    
@@ -49,16 +48,29 @@ if [ "$(echo "$LIBSSL" | grep -o 'libssl1\.0\.[0-9]:' | uniq | sort | wc -l)" -e
 fi
 
 # Create or update a non-root user to match UID/GID - see https://aka.ms/vscode-remote/containers/non-root-user.
+if [ "$USER_UID" = "" ]; then
+    USER_UID=1000
+fi 
+
+if [ "$USER_GID" = "" ]; then
+    USER_GID=1000
+fi 
+
+if [ "$USERNAME" = ""]; then
+    USERNAME=$(awk -v val=$USER_UID -F ":" '$3==val{print $1}' /etc/passwd)
+fi
+
 if id -u $USERNAME > /dev/null 2>&1; then
+    # User exists, update if needed
     if [ "$USER_GID" != "$(id -G $USERNAME)" ]; then 
         groupmod --gid $USER_GID $USERNAME 
         usermod --gid $USER_GID $USERNAME
     fi
-    
     if [ "$USER_UID" != "$(id -u $USERNAME)" ]; then 
         usermod --uid $USER_UID $USERNAME
     fi
 else
+    # Create user
     groupadd --gid $USER_GID $USERNAME
     useradd -s /bin/bash --uid $USER_UID --gid $USER_GID -m $USERNAME
 fi
--- script-library/common-redhat.sh
+++ script-library/common-redhat.sh
@@ -4,4 +4,16 @@
 # Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
 #-------------------------------------------------------------------------------------------------------------
 
-# Syntax: ./common-redhat.sh <username> <user UID> <user GID> <install zsh flag>
+# Syntax: ./common-redhat.sh <install zsh flag> <username> <user UID> <user GID>
+
+INSTALL_ZSH=$1
+USERNAME=$2
+USER_UID=$3
+USER_GID=$4
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo 'Script must be run a root. Use sudo or set "USER root" before running the script.'
+    exit 1
+fi
